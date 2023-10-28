cd758bbd86bdb0f88165f2dab336482f6e44594d
Krig Raseri <krigraseri@gmail.com>
2023-10-28 07:35:31 -0500

    Create RestRecipesProjectApplicationTests.java

---
+++ src/test/java/com/umbrella/recipes/RestRecipesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrella.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRecipesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
