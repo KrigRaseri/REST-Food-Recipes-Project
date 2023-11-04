4ae6af67c23397a6708a3e4e5699314fa137cc60
Krig Raseri <krigraseri@gmail.com>
2023-11-04 07:20:11 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.umbrella"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
@@ -22,19 +22,28 @@ repositories {
 }
 
 dependencies {
+    // Spring Implementation
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Database
     implementation("com.h2database:h2:2.2.224")
+    implementation("com.mysql:mysql-connector-j:8.2.0")
+    runtimeOnly("com.h2database:h2")
+
+    // Utility tools
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    runtimeOnly("com.h2database:h2")
     implementation("org.mapstruct:mapstruct:1.4.2.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
+
+    // Test
+    testImplementation("org.springframework.security:spring-security-test:6.1.5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
