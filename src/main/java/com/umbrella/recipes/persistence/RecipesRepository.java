e1318c0096c5ace893c722686c8fceb07c4577fd
Krig Raseri <krigraseri@gmail.com>
2023-11-13 05:35:16 -0600

    Add findByIdEager for tests
    Needed a new repo query method to eagerly load element collections ingredients and directions for tests.

--- src/main/java/com/umbrella/recipes/persistence/RecipesRepository.java
+++ src/main/java/com/umbrella/recipes/persistence/RecipesRepository.java
@@ -1,7 +1,9 @@
 package com.umbrella.recipes.persistence;
 
 import com.umbrella.recipes.model.RecipeModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,22 @@ import java.util.Optional;
 
 @Repository
 public interface RecipesRepository extends JpaRepository<RecipeModel, Long> {
+
+    @EntityGraph(attributePaths = {"ingredients"})
+    @Query("SELECT r FROM RecipeModel r LEFT JOIN FETCH r.ingredients WHERE r.recipeId = :id")
+    Optional<RecipeModel> findByIdWithIngredients(Long id);
+
+    @EntityGraph(attributePaths = {"directions"})
+    @Query("SELECT r FROM RecipeModel r LEFT JOIN FETCH r.directions WHERE r.recipeId = :id")
+    Optional<RecipeModel> findByIdWithDirections(Long id);
+
+    default Optional<RecipeModel> findByIdEager(Long id) {
+        Optional<RecipeModel> recipeWithIngredients = findByIdWithIngredients(id);
+        Optional<RecipeModel> recipeWithDirections = findByIdWithDirections(id);
+        recipeWithIngredients.ifPresent(recipeModel -> recipeModel.setDirections(recipeWithDirections.get().getDirections()));
+        return recipeWithIngredients;
+    }
+
     Optional<List<RecipeModel>> findByCategoryIgnoreCaseOrderByDateDesc(String category);
     Optional<List<RecipeModel>> findByNameContainingIgnoreCaseOrderByDateDesc(String name);
 }
